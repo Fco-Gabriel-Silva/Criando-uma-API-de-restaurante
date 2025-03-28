a9f9c7490a5b85071e8fc2ffb2fedfdc4dec6254
gustavohempel <hempel@gmail.com>
2025-03-28 18:13:38 -0300

    Refinando Abertura da Mesa

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { knex } from "@/database/knex";
 import { z } from "zod";
+import { AppError } from "@/utils/AppError";
 
 class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
@@ -10,6 +11,16 @@ class TablesSessionsController {
       });
 
       const { table_id } = bodySchema.parse(request.body);
+
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        // OBS: O uso de '.select()' é OPCIONAL. Caso não queira, basta passar logo o '.where() ...'
+        .where({ table_id })
+        .orderBy("opened_at", "desc")
+        .first();
+
+      if (session && !session.closed_at) {
+        throw new AppError("this table is already open");
+      }
 
       await knex<TablesSessionsRepository>("tables_sessions").insert({
         table_id,
